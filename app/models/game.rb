0f2a3ca9e68d32fa4cc3d85f1c3fdb23296e7c11
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2024-04-28 15:44:57 -0700

    add organizations scaffold

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,12 @@
 class Game < ApplicationRecord
   belongs_to :league
   has_one :location
+
+  class << self
+    def fetch_and_proccess_games
+      # Fetch games from external API
+
+      # Process games
+    end
+  end
 end
--- app/models/league.rb
+++ app/models/league.rb
@@ -1,4 +1,11 @@
 class League < ApplicationRecord
   has_many :teams
   has_many :games
+
+  def self.fetch_and_process_leagues
+    # Fetch leagues from external website
+
+    # Process leagues
+
+  end
 end
