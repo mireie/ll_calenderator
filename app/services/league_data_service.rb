a820440744041d35fb79a466f411aa366d356b4d
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2024-04-29 17:19:15 -0700

    Add LeagueDataService for fetching and processing external team data

---
+++ app/services/league_data_service.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# The LeagueDataService is responsible for providing data services for the Organization model
+class LeagueDataService
+  def initialize(league)
+    @league = league
+    @organization = league.organization
+  end
+
+  def fetch_and_process_external_team_data
+    fetch_teams.each { |team_data| create_or_update_team(team_data) }
+  end
+
+  private
+
+  def create_or_update_team(team_data)
+    team_attributes = team_attributes(team_data)
+    team = Team.find_or_initialize_by(external_id: team_attributes[:external_id])
+    team.update(team_attributes)
+    team.save
+  end
+
+  def external_id(team_data)
+    # /team/{team-id}/{team-name}
+    team_data.css("td.teamName a").first.attributes["href"].value.split("/")[2]
+  end
+
+  def team_attributes(team_data)
+    {
+      league: @league,
+      name: team_data.css("td.teamName a").first.text.strip,
+      external_id: external_id(team_data)
+    }
+  end
+
+  def fetch_teams
+    doc = ExternalDataService.fetch_and_parse_external_data(standings_url)
+    doc = doc.css("table.divisionStandings tbody tr")
+    doc.to_ary.select { |team_data| team_data.css("td.teamName a").any? }
+  end
+
+  def standings_url
+    "#{@organization.base_url}/league/#{@league.external_id}/standings"
+  end
+end
