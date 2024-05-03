60655b6417ba38d20c8da88cda8f0a5e6eb2bab2
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2024-05-02 21:31:58 -0700

    simple filtering

--- app/controllers/public_files_controller.rb
+++ app/controllers/public_files_controller.rb
@@ -1,17 +1,29 @@
-# app/controllers/public_files_controller.rb
+# frozen_string_literal: true
+
 class PublicFilesController < ApplicationController
   def index
-    @files = Dir.glob("public/**/*.ics")
-    @files = []
+    @files = get_files
+    # fuzzy search
+    return @files unless params[:team_name].present?
+
+    @files = @files.select { |file| file[:team_name].downcase.include?(params[:team_name].downcase) }
+  end
+
+  private
+
+  def get_files
+    files = []
+
     Dir.glob("public/**/*.ics").each do |file|
       team_id = file.split("/")[-1].split(".")[0]
       team = Team.find_by(external_id: team_id)
-      @files << {
+      files << {
         team_id:,
         team_name: team.name,
         league_name: team.league.title,
         path: file
       }
     end
+    files
   end
 end
