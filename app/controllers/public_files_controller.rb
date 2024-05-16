badb63ca5ed3eaeae0f82d1b9637e4b0e0d58281
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2024-05-16 16:08:21 -0700

    support all games ics files

--- app/controllers/public_files_controller.rb
+++ app/controllers/public_files_controller.rb
@@ -16,13 +16,23 @@ class PublicFilesController < ApplicationController
 
     Dir.glob("public/**/*.ics").each do |file|
       team_id = file.split("/")[-1].split(".")[0]
-      team = Team.find_by(external_id: team_id)
-      files << {
-        team_id:,
-        team_name: team.name,
-        league_name: team.league.title,
-        path: file
-      }
+      league_id = file.split("/")[-2]
+      if team_id == "games"
+        files << {
+          team_id: nil,
+          team_name: "All Games",
+          league_name: League.find(league_id).title,
+          path: file
+        }
+      else
+        team = Team.find_by(external_id: team_id)
+        files << {
+          team_id:,
+          team_name: team.name,
+          league_name: team.league.title,
+          path: file
+        }
+      end
     end
     files
   end
