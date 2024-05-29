ac9c2941d6624b1c756a420e2074f999d951e3ed
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2024-05-29 11:39:46 -0700

    remove sort

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -9,7 +9,7 @@ class LeaguesController < ApplicationController
   def index
     @leagues = League.all
     @sports = League.distinct.pluck(:sport)
-    @days = League.distinct.pluck(:days).flatten.uniq.sort
+    @days = League.distinct.pluck(:days).flatten.uniq
 
     process_request
     @leagues = @leagues.sort_by(&:start_date)
