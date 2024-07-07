d89acc9308b82c84e63f8ac285722e3ac2614353
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2024-07-06 19:04:27 -0700

    somehow this missed the PR

---
+++ db/migrate/20240707015437_add_external_id_to_locations.rb
@@ -0,0 +1,5 @@
+class AddExternalIdToLocations < ActiveRecord::Migration[7.0]
+  def change
+    add_column :locations, :external_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_06_212613) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_07_015437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,11 +60,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_06_212613) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "external_id"
     t.float "latitude"
     t.float "longitude"
     t.string "country"
     t.boolean "verified", default: false
+    t.string "external_id"
     t.index ["latitude", "longitude"], name: "index_locations_on_latitude_and_longitude"
   end
 
