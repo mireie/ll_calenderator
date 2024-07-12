9d69c1a73f4eced949f1e4478770834ce8f3d1bb
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2024-07-12 11:18:05 -0700

    remove migration

--- db/migrate/20240712005315_remove_defunct_columns.rb
+++
@@ -1,8 +0,0 @@
-class RemoveDefunctColumns < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :leagues, :base_url
-    remove_column :leagues, :schedule_path
-    remove_column :leagues, :standings_path
-    remove_column :leagues, :parsing_strategy
-  end
-end
