417494a18726671ca119c45299d0f98a835fcda6
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2024-07-15 10:03:39 -0700

    add timezone to icalendar

--- app/models/team_calendar.rb
+++ app/models/team_calendar.rb
@@ -3,17 +3,23 @@
 class TeamCalendar
   attr_reader :team, :games
 
-  require "icalendar/tzinfo"
-
   def initialize(team)
     @team = team
   end
 
   def to_ical
     cal = Icalendar::Calendar.new
+    cal.add_timezone(timezone.ical_timezone(Time.zone.now))
     @team.games.each do |game|
       cal.add_event(game[:game].to_ical(game[:role]))
     end
     cal.to_ical
+  end
+
+  private
+
+  def timezone
+    tzid = "America/Los_Angeles"
+    TZInfo::Timezone.get(tzid)
   end
 end
