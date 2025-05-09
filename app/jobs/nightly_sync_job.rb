3e2898b71772b32ad466f7cdc3bc5c2b703c454c
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2025-05-09 07:51:17 -0700

    Refactor NightlySyncJob to streamline league update processing
    - Removed unnecessary array accumulation for league update jobs.
    - Simplified the execution of SyncLeagueDetailsJob and SyncLeagueScheduleJob for upcoming and active leagues.

--- app/jobs/nightly_sync_job.rb
+++ app/jobs/nightly_sync_job.rb
@@ -2,14 +2,13 @@
 
 class NightlySyncJob < ApplicationJob
   def perform
-    league_update_jobs = []
-    league_update_jobs << League.upcoming.each do |league|
+    League.upcoming.each do |league|
       SyncLeagueDetailsJob.perform_later(league)
       SyncLeagueScheduleJob.perform_later(league)
     end
-    league_update_jobs << League.active.each do |league|
+
+    League.active.each do |league|
       SyncLeagueDetailsJob.perform_later(league)
     end
-    ActiveJob.perform_all_later(league_update_jobs)
   end
 end
